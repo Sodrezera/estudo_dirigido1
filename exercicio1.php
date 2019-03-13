37ebc06370ec23b9f5463553410023b52f30a8c0
Sodrezera <48135751+Sodrezera@users.noreply.github.com>
2019-03-13 16:16:24 -0300

    Update exercicio1.php

--- exercicio1.php
+++ exercicio1.php
@@ -1,3 +1,6 @@
-# Repositório para desenvolvimento de estudo dirigido - Programaçãotudo_dirigido1
-2
+<?php
+    print "Quantos metro você deseja converter?\n";
+    $metro = (int)fgets(STDIN);
+    $convertimento = $metro*1000;
 
+        print "Convertido para milímitros ficara: $convertimento\n";
