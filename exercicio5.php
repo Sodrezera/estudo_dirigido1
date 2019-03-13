e6ca997f6f13108d1a3dff23ddcfc17c17f6a364
Sodrezera <48135751+Sodrezera@users.noreply.github.com>
2019-03-13 18:10:01 -0300

    Update exercicio5.php

--- exercicio5.php
+++ exercicio5.php
@@ -1,7 +1,8 @@
 <?php  
-    print "Digite quantos quilômetros deseja percorrer?\n";
-    $km = (int) fgets(STDIN);  
+    print "Digite quantos quilômetros deseja percorrer:\n";
+    $km = (int) fgets(STDIN);    
     print "Digite a velocidade média para a viajem:\n";
     $velo = (int) fgets(STDIN);
-    $tempo = (float)$km/$velo;
-    print "O tempo esperado para a viajem sera $tempo horas.\n\n";
+    $tempo = $km/$velo;
+    $tempo1= $tempo*60;
+    print "O tempo esperado para a viajem sera $tempo1 minutos.\n\n";
