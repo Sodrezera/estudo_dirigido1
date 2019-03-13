bfb1f356d145cec681d7c32ae02a14452b3dbc1b
Sodrezera <48135751+Sodrezera@users.noreply.github.com>
2019-03-13 16:20:06 -0300

    Update exercicio1.php

--- exercicio1.php
+++ exercicio1.php
@@ -2,5 +2,4 @@
     print "Quantos metro você deseja converter?\n";
     $metro = (int)fgets(STDIN);
     $convertimento = $metro*1000;
-
         print "Convertido para milímitros ficara: $convertimento\n";
