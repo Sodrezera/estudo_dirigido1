80962f9ab7ac40e754dfe822ed31220a925e3c18
Sodrezera <48135751+Sodrezera@users.noreply.github.com>
2019-03-13 16:17:09 -0300

    Create exercicio2.php

---
+++ exercicio2.php
@@ -0,0 +1,9 @@
+<?php
+    print "De uma quantidade de dias:\n";
+    $dias = (int) fgets(STDIN);
+
+    $hora = $dias*24;
+    $minutos = $hora*60;
+    $segundos = $minutos*60;
+
+    print "O seu dia tera $hora horas, $minutos minutos, e um total de $segundos segundo.\n\n";
