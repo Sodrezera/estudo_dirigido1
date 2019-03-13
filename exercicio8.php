a88055c9962a28fc97d6bc74fc266931760535b1
Sodrezera <48135751+Sodrezera@users.noreply.github.com>
2019-03-13 17:22:07 -0300

    Create exercicio8.php

---
+++ exercicio8.php
@@ -0,0 +1,9 @@
+<?php  
+    print "Digite quantos cigarros você fuma por dia:\n";
+    $pordia = (int) fgets(STDIN);    
+    print "Digite por quantos anos você fuma:\n";
+    $anos = (int) fgets(STDIN);
+    $diastotais = $anos*365;
+    $cigarrototais = $diastotais*$pordia;
+    $tempoperdido = (($cigarrototais*10)/24)/60;
+    print "Você perdeu $tempoperdido dias da sua vida.\n\n";
